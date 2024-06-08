f6c47a1b098c4f47dc9213e69651b757339caaf8
akhilesh <akhi23waghmare@gmail.com>
2024-06-08 16:55:50 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -46,6 +48,8 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
       </body>
+      <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
